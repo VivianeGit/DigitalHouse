276fbe38d58daa7002beff6645e88f9aa2360bff
VivianeGit <vivianecipriano.a@gmail.com>
2020-10-07 18:40:05 -0300

    Adiciona exercícios de Android

---
+++ Android/Aula16/Exerccio1Casa/app/src/main/java/com/example/exerccio1_casa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.exerccio1_casa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
