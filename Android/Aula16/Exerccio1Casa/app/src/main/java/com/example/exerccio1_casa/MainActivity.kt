489da8d394a2f9c792e00fc3f5d4b82d70baebf7
VivianeGit <vivianecipriano.a@gmail.com>
2020-10-08 19:18:47 -0300

    Adiciona arquivos

--- Android/Aula16/Exerccio1Casa/app/src/main/java/com/example/exerccio1_casa/MainActivity.kt
+++ Android/Aula16/Exerccio1Casa/app/src/main/java/com/example/exerccio1_casa/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.exerccio1_casa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnExibir.setOnClickListener{
+         txtTexto.text = "Meu nome é ${acao.text}! "
+        }
     }
 }
--- Exercícios/src/Lista1/Exercício3_Lista1.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-    println("Digite sua altura: ")
-    val h = readLine()!!.toDouble()
-    println("Para homens ${(72.7 * h) - 58}!")
-    println("Para mulheres ${(62.1 * h) - 44.7}!")
-}
