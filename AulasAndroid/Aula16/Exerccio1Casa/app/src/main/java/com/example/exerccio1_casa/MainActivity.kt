1337ed8c6477fb22c902c05732166cb9067c1d2f
VivianeGit <vivianecipriano.a@gmail.com>
2020-10-08 21:17:50 -0300

    Adciona exercícios

--- AulasAndroid/Aula16/Exerccio1Casa/app/src/main/java/com/example/exerccio1_casa/MainActivity.kt
+++ AulasAndroid/Aula16/Exerccio1Casa/app/src/main/java/com/example/exerccio1_casa/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.exerccio1_casa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val  MEUNOME = "Meu nome é"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnExibir.setOnClickListener{
-         txtTexto.text = "Meu nome é ${acao.text}! "
+        btnExibir.setOnClickListener {
+            val resultadoDoNome = etAcao.text.toString()
+
+            when {
+                resultadoDoNome.isNotEmpty() -> {
+                    txtTexto.visibility = View.VISIBLE
+
+                    txtTexto.text = ("$MEUNOME $resultadoDoNome")
+                }
+                else-> {
+                    etAcao.error = "Preencha o campo ai meu"
+                }
+            }
+        }
+
+        btnRestart.setOnClickListener {
+            txtTexto.visibility = View.GONE
         }
     }
 }
